669786c1440da540513aef3f1dc36bcf10a45c7d
lucasdxvid <athl12311@gmail.com>
2023-04-13 16:00:53 -0300

    useReducer

--- src/08-useReducer/todoReducer.js
+++ src/08-useReducer/todoReducer.js
@@ -1,9 +1,12 @@
-export const todoReducer = (initialState, action) => {
+export const todoReducer = (initialState = [], action) => {
   switch (action.type) {
-    case "ABC": // Si la accion es ABC, hacemos...
-      throw new Error("Action.type = ABC no esta implentada"); // Esto debemos como seguridad cuando todavia no implementamos X accion
-
+    case "[TODO] Add-Todo": // Si la accion es [TODO] Add-Todo, hacemos...
+      return [...initialState, action.payload];
+    case "[TODO] Remove-Todo": //Hacemos un caso similar pero para borrar
+      return initialState.filter(todo => todo.id !== action.payload); //El filter nos regresa un nuevo arreglo con los que cumplan la condicion
     default:
       return initialState; // Si no tira un error debe de tirar el initialState (Tambien podemos poner el initialState = [] en nuestro argumento pero ahora no)
   }
 };
+
+// Esto iba abajo del primer case: throw new Error("Action.type = ABC no esta implentada"); // Esto debemos como seguridad cuando todavia no implementamos X accion
